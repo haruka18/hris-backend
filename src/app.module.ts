7592caf0d0c02406840b50cc3a4e21964f92feea
hendra purnomo <hendrapurnomo39@gmail.com>
2026-07-06 15:47:14 +0700

    chore: configure backend environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      cache: true,
+
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .required(),
+
+        APP_NAME: Joi.string().required(),
+
+        APP_PORT: Joi.number().port().required(),
+
+        API_PREFIX: Joi.string().required(),
+
+        API_VERSION: Joi.string().required(),
+
+        FRONTEND_URL: Joi.string().uri().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.getOrThrow<number>('APP_PORT');
+  const apiPrefix = configService.getOrThrow<string>('API_PREFIX');
+  const apiVersion = configService.getOrThrow<string>('API_VERSION');
+  const frontendUrl = configService.getOrThrow<string>('FRONTEND_URL');
+
+  app.setGlobalPrefix(`${apiPrefix}/${apiVersion}`);
+
+  app.enableCors({
+    origin: frontendUrl,
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableShutdownHooks();
+
+  await app.listen(port);
+
+  console.log(
+    `HRIS API running on http://localhost:${port}/${apiPrefix}/${apiVersion}`,
+  );
 }
-bootstrap();
+
+void bootstrap();
